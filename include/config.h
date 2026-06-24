e2085d3954d5a9ee6c5622a55a68f57e9de1bbef
Samuel Silveira <samuelsilveira100@gmail.com>
2026-06-24 15:29:14 -0300

    adiciona estrutura de pastas inicial

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+typedef struct {
+    int     num_veiculos;
+    int     tick_ms;
+    char    *mapa_path;
+} Config;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+    Config cfg = {0};
+    int opt;
+
+    while ((opt = getopt(argc, argv, "v:t:m:h")) != -1) {
+        switch (opt) {
+            case 'v': cfg.num_veiculos = atoi(optarg); break;
+            case 't': cfg.tick_ms      = atoi(optarg); break;
+            case 'm': cfg.mapa_path    = optarg;       break;
+            case 'h':
+            default:
+                fprintf(stderr, "Uso: ./simulador -v <veiculos> -t <tick_ms> [-m <mapa.txt>]\n");
+                exit(opt == 'h' ? EXIT_SUCCESS : EXIT_FAILURE);
+        }
+    }
+
+    if (cfg.num_veiculos <= 0 || cfg.tick_ms <= 0) {
+        fprintf(stderr, "Erro: -v e -t são obrigatórios.\n");
+        fprintf(stderr, "Uso: ./simulador -v <veiculos> -t <tick_ms> [-m <mapa.txt>]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Veiculos: %d | Tick: %dms | Mapa: %s\n",
+           cfg.num_veiculos, cfg.tick_ms,
+           cfg.mapa_path ? cfg.mapa_path : "(nenhum)");
+
+    return EXIT_SUCCESS;
+}
